5bcff387237261be6d46c11ac434ece61d1411a9
jessevandenberg <156073960+jessevandenberg@users.noreply.github.com>
2025-11-13 00:42:21 +0100

    oproepen achtergrond fantasyteam

---
+++ app/fantasyteam/fantasyteam.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Achtergrond from "@/components/Achtergrond";
+
+const FantasyTeam = () => {
+  return (
+    <div className="relative min-h-screen bg-[#0b0b0b] overflow-hidden">
+      {/* Achtergrond met V-vormige lichtbundels */}
+      <Achtergrond/>
+      
+      
+      
+      {/* Content boven de achtergrond */}
+      <div className="relative z-10">
+        {/* Content komt hier */}
+      </div>
+    </div>
+  );
+};
+
+export default FantasyTeam;
